c5f249c3a6dff00268c325328edcf54d06f3b5da
OksanaPros <oksanaprosniakova@yandex.ru>
2025-03-19 10:52:45 +0100

    test

--- homework3/test_task1.py
+++ homework3/test_task1.py
@@ -5,9 +5,9 @@ def test_cache_works():
     """Testing that cache function caches function calls several times"""
 
     @cache(times=2)
-    def f(a):
-        return a
+    def f():
+        return 2**123
 
-    v1, v2, v3, v4 = (f(2**123) for _ in range(4))
+    v1, v2, v3, v4 = (f() for _ in range(4))
 
     assert (v1 is v2) and (v1 is v3) and not (v1 is v4)
